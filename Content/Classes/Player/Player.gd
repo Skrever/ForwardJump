cef6c9ad1d0a07d56f6dbd0b5e137076c66e920b
Skrever <73542424+Skrever@users.noreply.github.com>
2024-07-24 23:02:06 +0300

    Добавлена физика колонны и игрока

--- Content/Classes/Player/Player.gd
+++ Content/Classes/Player/Player.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends CharacterBody3D
 
 var move : bool = false
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _physics_process(delta):
+	if (!is_on_floor()):
+		velocity.y = -10
+	move_and_slide()
